d0a5db3749a72fbf31686f8158909910c343c048
FatChicken277 <1637@holbertonschool.com>
2020-11-13 12:30:06 -0500

    Add user model
    Because:
        - To have control over the arrival of data
        - To have control when interacting with the database
    
    added initial structure of user data model

---
+++ backend/models/user.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+var (
+	//ErrMissingUsername indicates that the username is missing
+	ErrMissingUsername = errors.New("'username' cannot be empty")
+)
+
+// UserModel defines the model for users
+type UserModel struct {
+	UserID   int    `json:"user_id"`
+	Username string `json:"username"`
+	Score    int    `json:"score"`
+}
+
+// UserList defines the structure of a user list
+type UserList struct {
+	Users []UserModel `json:"users"`
+}
+
+// VerifyParams is used to verify the user parameters
+func (u *UserModel) VerifyParams() error {
+	if u.Username == "" {
+		return ErrMissingUsername
+	}
+
+	return nil
+}
+
+// ToResponse is used to convert the user object into a JSON response
+func (u *UserModel) ToResponse() ([]byte, error) {
+	resp, err := json.Marshal(u)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
---
+++ backend/models/user_test.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUserVerifyParams(t *testing.T) {
+	c := require.New(t)
+
+	newUser := UserModel{
+		UserID:   12,
+		Username: "FatChicken",
+		Score:    32,
+	}
+
+	err := newUser.VerifyParams()
+	c.NoError(err)
+
+	newUser.Username = ""
+	err = newUser.VerifyParams()
+	c.Error(err)
+	c.Equal(ErrMissingUsername, err)
+}
+
+func TestUserToResponse(t *testing.T) {
+	c := require.New(t)
+
+	var newUser1 UserModel
+	newUser2 := UserModel{
+		UserID:   12,
+		Username: "FatChicken",
+		Score:    32,
+	}
+
+	resp, err := newUser2.ToResponse()
+	c.NoError(err)
+
+	json.Unmarshal(resp, &newUser1)
+	c.Equal(newUser1, newUser2)
+}
